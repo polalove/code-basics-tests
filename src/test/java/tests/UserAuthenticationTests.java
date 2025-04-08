5b9021e9df00d2463de917fe0f116d5e7f6d2079
Ivan Novikov <pola2288@yandex.ru>
2025-04-08 16:20:07 +0300

    add parametrized tests

--- src/test/java/tests/UserAuthenticationTests.java
+++ src/test/java/tests/UserAuthenticationTests.java
@@ -4,7 +4,13 @@ import data.TestData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import pages.AuthenticationPage;
+
+import java.util.stream.Stream;
 
 import static data.Credentials.LOGIN;
 import static data.Credentials.PASSWORD;
@@ -25,14 +31,23 @@ public class UserAuthenticationTests extends TestBase {
         authenticationPage.checkAlertMessage("Внутри лучше чем снаружи. Запоминаем прогресс и ведем к свету");
     }
 
-    @Test
-    @DisplayName("Авторизация с неверным паролем")
-    void loginWithIncorrectPasswordTest() {
+    @ParameterizedTest(name = "Авторизация с неверным паролем: {0}")
+    @MethodSource("provideWrongPasswords")
+    @DisplayName("Авторизация с неверными паролями")
+    void loginWithIncorrectPasswordTest(String password) {
         authenticationPage.openLoginPage(0);
         authenticationPage.enterEmail(LOGIN);
-        authenticationPage.enterPassword(TestData.getRandomWrongPassword());
+        authenticationPage.enterPassword(password);
         authenticationPage.clickSubmitButton();
         authenticationPage.checkUrlCorrection("https://code-basics.com/ru/session/new");
+    }
+
+    private static Stream<Arguments> provideWrongPasswords() {
+        return Stream.of(
+                Arguments.of(TestData.getRandomWrongPassword()),
+                Arguments.of(TestData.getRandomWrongPassword()),
+                Arguments.of("invalid_password_123")
+        );
     }
 
     @Test
@@ -53,24 +68,39 @@ public class UserAuthenticationTests extends TestBase {
         authenticationPage.checkAlertMessage("Мы отправили вам письмо с инструкциями по восстановлению пароля");
     }
 
-    @Test
-    @DisplayName("Восстановление пароля с невалидным email")
-    void recoverPasswordWithInvalidEmailTest() {
+    @ParameterizedTest(name = "Восстановление пароля с невалидным email: {0}")
+    @ValueSource(strings = {
+            "invalid",
+            "test@",
+            "@domain.com",
+            "noatsign"
+    })
+    @DisplayName("Восстановление пароля с невалидными email")
+    void recoverPasswordWithInvalidEmailTest(String email) {
         authenticationPage.openLoginPage(0);
         authenticationPage.clickRecoveryLink();
-        authenticationPage.enterRecoveryEmail(TestData.getRandomInvalidEmail());
+        authenticationPage.enterRecoveryEmail(email);
         authenticationPage.clickSubmitButton();
         authenticationPage.checkAlertMessage("В форме есть ошибки");
     }
 
-    @Test
-    @DisplayName("Восстановление пароля с несуществующим email")
-    void recoverPasswordWithNonExistentEmailTest() {
+    @ParameterizedTest(name = "Восстановление пароля с несуществующим email: {0}")
+    @MethodSource("provideNonExistentEmails")
+    @DisplayName("Восстановление пароля с несуществующими email")
+    void recoverPasswordWithNonExistentEmailTest(String email) {
         authenticationPage.openLoginPage(0);
         authenticationPage.clickRecoveryLink();
-        authenticationPage.enterRecoveryEmail(TestData.getRandomNonExistentEmail());
+        authenticationPage.enterRecoveryEmail(email);
         authenticationPage.clickSubmitButton();
         authenticationPage.checkAlertMessage("В форме есть ошибки");
+    }
+
+    private static Stream<Arguments> provideNonExistentEmails() {
+        return Stream.of(
+                Arguments.of(TestData.getRandomNonExistentEmail()),
+                Arguments.of("nonexistent1@example.com"),
+                Arguments.of("notregistered@test.org")
+        );
     }
 
     @Test
